98bb07de82c076caad8b44db01b263f9ee82ee42
Herman <herman.lin@foxmail.com>
2018-10-23 15:37:10 -0700

    Added Flutter Skeleton

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PotatoMonchers
+# Pototto Navigator
+
+A new Flutter project. Right now you can click search icon and enter text and it will search for Places in LA.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Pototto - Dank Trip Planner',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatefulWidget {
+  // ExamplePage({ Key key }) : super(key: key);
+  @override
+  _ExamplePageState createState() => new _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+ // final formKey = new GlobalKey<FormState>();
+ // final key = new GlobalKey<ScaffoldState>();
+  final TextEditingController _filter = new TextEditingController();
+  final dio = new Dio();
+  String _searchText = "";
+  List names = new List();
+  List filteredNames = new List();
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text('Search for Places in Los Angeles' );
+
+  _ExamplePageState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+          _getNames(_filter.text);
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    _getNames("anime");
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+      resizeToAvoidBottomPadding: false,
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    if (!(_searchText.isEmpty)) {
+      List tempList = new List();
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i].toLowerCase().contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
+    return ListView.builder(
+      itemCount: names == null ? 0 : filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(filteredNames[index]),
+          onTap: () => print(filteredNames[index]),
+        );
+      },
+    );
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+            prefixIcon: new Icon(Icons.search),
+            hintText: 'Search...'
+          ),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text( 'Search Places in Los Angeles' );
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
+  }
+
+  void _getNames(String target) async {
+    final uri = new Uri.https('maps.googleapis.com', '/maps/api/place/textsearch/json', 
+                             {"query": target + " in Los Angeles", "key":"AIzaSyAwnXWH-qrRpBWraATnVVyHxKYuRSZEQ8M"});
+    final response = await dio.get(uri.toString());
+    List tempList = new List();
+    if (response.data['results'] != Null) {
+      for (int i = 0; i < response.data['results'].length; i++) {
+        tempList.add(response.data['results'][i]['name']);
+      }
+    }
+    setState(() {
+      names = tempList;
+      filteredNames = names;
+    });
+  }
+}
